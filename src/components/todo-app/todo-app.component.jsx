6c9a5fe02c7ca61a171bbcde8f85775432aaa164
Kendi <muriungikendileila@gmail.com>
2025-02-02 17:46:40 +0300

    adding sass to my todo app component

--- src/components/todo-app/todo-app.component.jsx
+++ src/components/todo-app/todo-app.component.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
+
 import TaskInput from "../task-input/task-input.component";
+import './todo-app.scss';
 
 const Todo = () => {
     const [tasks, setTasks] = useState([]);
@@ -10,14 +12,22 @@ const Todo = () => {
     }
 
     return (
-        <div>
-            <TaskInput addTask={addTask} cancelTask={() => {}}/>
-            <ul>
+        <div className="todo-container">
+            <div className="task-input-section">
+                <TaskInput addTask={addTask} cancelTask={() => {}}/>
+            </div>
+            <div className="task-list-section">
+               <ul>
                 {tasks.map((task, index) => {
                     return (
-                    <li key={index}>{task.task} - {task.start} to {task.end}</li>
-                )})}
-            </ul>
+                        <li key={index}>{task.task} - {task.start} to {task.end}</li>
+                    )})}
+                </ul> 
+            </div>
+            <div className="analytics-section">
+                <h3>Analytics</h3>
+            </div>
+            
             
         </div>
     );
