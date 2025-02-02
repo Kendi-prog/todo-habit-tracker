fc0e41cc6c03fd888f91279f0d8b6adc994c6a6b
Kendi <muriungikendileila@gmail.com>
2025-02-02 18:28:29 +0300

    updating my task input component by adding extra divs and the pinned and category-box sections

--- src/components/task-input/task-input.component.jsx
+++ src/components/task-input/task-input.component.jsx
@@ -1,30 +1,29 @@
 import React, { use, useState } from "react";
 import Dropdown from "../dropdown/dropdown.component";
 
-const TaskInput = ({ addTask, removeTask }) => {
+const TaskInput = ({ addTask, removeTask, personalCount, officialCount }) => {
     const [task, setTask] = useState('');
     const [startDate, setStartDate] = useState('');
     const [endDate, setEndDate] = useState('');
     const [startTime, setStartTime] = useState('08:00');
     const [endTime, setEndTime] = useState('10:00');
-    // const [frequency, setFrequency] = useState('no repeat');
-    // const [category, setCategory] = useState('Personal');
-    // const [notes, setNotes] = useState('');
+    const [frequency, setFrequency] = useState('no repeat');
+    const [category, setCategory] = useState('Personal');
+    const [notes, setNotes] = useState('');
 
     const handleAddTask = () => {
         if(task && startDate && startTime) {
             const newTask = {
                 task, 
                 start: `${startDate} ${startTime}`,
-                end: `${endDate} ${endTime}`  //frequency, category, notes
+                end: `${endDate} ${endTime}`,
+                frequency,
+                category,
+                notes,
             };
 
             addTask(newTask);
-            resetForm();
-
-                // setFrequency('no repeat');
-            // setCategory('Personal');
-            // setNotes('');
+            resetForm(); 
         }
     }
 
@@ -34,75 +33,112 @@ const TaskInput = ({ addTask, removeTask }) => {
         setEndDate('');
         setStartTime('08:00');
         setEndTime('10:00');
+        setFrequency('no repeat');
+        setCategory('Personal');
+        setNotes('');
     }
 
            
-        
-
-    // const frequencyOptions = ['no repeat', 'daily', 'weekly', 'monthly', 'yearly'];
-    // const categoryOptions = ['Personal', 'Official'];
+    const frequencyOptions = ['no repeat', 'daily', 'weekly', 'monthly', 'yearly'];
+    //const categoryOptions = ['Personal', 'Official'];
 
     return (
-        <div>
-            <h2>To-Do List</h2>
-            <input 
-            type="text"
-            value={task}
-            onChange={(e) => setTask(e.target.value)}
-            placeholder="Task name"/>
+        <div className="task-input">
+            <div className="category-counts">
+                <div className="category-box">
+                    <h3>Personal</h3>
+                    <p>{personalCount} </p>
+                </div>
+                <div className="category-box">
+                    <h3>Official</h3>
+                    <p>{officialCount} </p>
+                </div>
+            </div>
+            <div className="pinned-tasks">
+                <Dropdown 
+                options={frequencyOptions}
+                selected={frequency}
+                setSelected={setFrequency}
+                />
 
-            <label>
-                Start Date:
-                <input
-                type="date"
-                value={startDate}
-                onChange={(e) => setStartDate(e.target.value)} />
-            </label>
+                <span>Pinned</span>
+            </div>
+            <div className="input-area">
+                <h2>Add new task</h2>
+                <input 
+                type="text"
+                value={task}
+                onChange={(e) => setTask(e.target.value)}
+                placeholder="Task name"
+                />
 
-            <label>
-                Start Time:
-                <input
-                type="time"
-                value={startTime}
-                onChange={(e) => setStartTime(e.target.value)} />
-            </label>
+                <div className="date-time">
+                    <label>
+                        Start Date:
+                        <input
+                        type="date"
+                        value={startDate}
+                        onChange={(e) => setStartDate(e.target.value)} 
+                        />
+                    </label>
+                    <label>
+                        Start Time:
+                        <input
+                        type="time"
+                        value={startTime}
+                        onChange={(e) => setStartTime(e.target.value)} 
+                        />
+                    </label>
+                </div>
+                <p className="to-text">To</p>
+                <div className="date-time">
+                    <label>
+                        End Date:
+                        <input
+                        type="date"
+                        value={endDate}
+                        onChange={(e) => setEndDate(e.target.value)} 
+                        />
+                    </label>
+                    <label>
+                        End Time:
+                        <input
+                        type="time"
+                        value={endTime}
+                        onChange={(e) => setEndTime(e.target.value)} 
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        <input 
+                        type="radio"
+                        value="Personal"
+                        checked={category === Personal}
+                        onChange={() => setCategory('Personal')}
+                        />
+                        Personal
+                    </label>
+                    <label>
+                        <input 
+                        type="radio"
+                        value="Official"
+                        checked={category === Official}
+                        onChange={() => setCategory('Official')}
+                        />
+                        Official
+                    </label>
+                </div>
 
-            <label>
-                End Date:
-                <input
-                type="date"
-                value={endDate}
-                onChange={(e) => setEndDate(e.target.value)} />
-            </label>
+                <textarea 
+                    value={notes}
+                    onChange={(e) => setNotes(e.target.value)}
+                    placeholder="Add note"
+                    />
 
-            <label>
-                End Time:
-                <input
-                type="time"
-                value={endTime}
-                onChange={(e) => setEndTime(e.target.value)} />
-            </label>
-
-
-            {/*<Dropdown 
-            label="Frequency"
-            options={frequencyOptions}
-            selected={frequency}
-            setSelected={setFrequency}/>
-
-            <Dropdown 
-            label="Category"
-            options={categoryOptions}
-            selected={category}
-            setSelected={setCategory}/> */}
-
-            {/* <textarea 
-            value={notes}
-            onChange={(e) => setNotes(e.target.value)}
-            placeholder="Add note"/>*/}
-
-            <button onClick={handleAddTask}>Save</button>
-            <button onClick={removeTask}>Cancel</button> 
+                <button onClick={handleAddTask}>Save</button>
+                <button onClick={removeTask}>Cancel</button> 
+            </div>    
         </div>
     );
 }
