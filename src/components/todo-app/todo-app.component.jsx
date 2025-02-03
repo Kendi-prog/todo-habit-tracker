f145dc7d4cea096e2c9b49c6fc087e17c2d2f556
Kendi <muriungikendileila@gmail.com>
2025-02-03 17:29:14 +0300

    implementing the personal and official counts for the category-box

--- src/components/todo-app/todo-app.component.jsx
+++ src/components/todo-app/todo-app.component.jsx
@@ -5,16 +5,43 @@ import './todo-app.scss';
 
 const Todo = () => {
     const [tasks, setTasks] = useState([]);
+    const [personalCount, setPersonalCount] = useState(0);
+    const [officialCount, setOfficialCount] = useState(0);
 
     const addTask = (newTask) => {
         setTasks([...tasks, newTask]);
+        updateCounts(newTask.category);
     
+    }
+
+    const updateCounts = (category) => {
+        if(category === 'Personal'){
+            setPersonalCount(prevCount => prevCount + 1);
+        } else if (category === 'Official') {
+            setOfficialCount(prevCount => prevCount + 1);
+        }
+    }
+
+    const removeTask = (taskToRemove) => {
+        const updatedTasks = tasks.filter(task => task !== taskToRemove);
+        setTasks(updatedTasks);
+
+        if(taskToRemove.category === 'Personal'){
+            setPersonalCount(prevCount => Math.max(prevCount - 1, 0));
+        } else if (taskToRemove.category === 'Official') {
+            setOfficialCount(prevCount => Math.max(prevCount - 1, 0));
+        }
     }
 
     return (
         <div className="todo-container">
             <div className="task-input-section">
-                <TaskInput addTask={addTask} cancelTask={() => {}}/>
+                <TaskInput 
+                addTask={addTask} 
+                removeTask={removeTask}
+                personalCount={personalCount}
+                officialCount={officialCount}
+                />
             </div>
             <div className="task-list-section">
                <ul>
