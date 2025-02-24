1bf374c06c0300912954fa711ccc93fa5cad99e9
Kendi <muriungikendileila@gmail.com>
2025-02-24 11:44:38 +0300

    installing firebase and implementing thaddTask function in firestore

--- src/ToDo App/components/todo.main/todo.main.component.jsx
+++ src/ToDo App/components/todo.main/todo.main.component.jsx
@@ -1,17 +1,49 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
+import { collection, addDoc, getDocs } from "firebase/firestore";
 
 import TaskManagement from "../task-management/task-management.component";
 import TaskInput from "../task-input/task-input.component";
 import { TodoMainContainer, TaskInputArea, TaskListArea, AnalyticsArea } from "./todo.main.styles";
 import TaskCategorySummary from "../task-category-summary/task-category-summary.component";
+import db from "../../../utils/firebase";
 
 const ToDo = () => {
     const [tasks, setTasks] = useState([]);
     const [editingTask, setEditingTask] = useState(null);
 
-    const addTask = (newTask) => {
-        setTasks([...tasks, { ...newTask, id:Date.now(), completed:false}]);
+
+    const addTask = async (newTask) => {
+      try {
+        const docRef = await addDoc(collection(db, "tasks"), {
+          ...newTask,
+          completed: false,
+          createdAt: new Date()
+        });
+
+        console.log("Task added with ID: ", docRef.id);
+        fetchTasks();
+       
+      } catch (e) {
+        console.error("Error adding task: ", e);
       }
+    }
+
+    const fetchTasks = async () => {
+      try {
+        const querySnapShot = await getDocs(collection(db, "tasks"));
+        const tasksData = querySnapShot.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data()
+        }));
+        setTasks(tasksData);
+      } catch (e) {
+        console.error("Error fetching tasks: ", e);
+      }
+    }
+
+    useEffect(() => {
+      fetchTasks();
+    }, []);
 
     const deleteTask = (id) => {
       setTasks(tasks.filter(task => task.id !== id));
---
+++ src/utils/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAOb5T9J95Lhpn-1jwOkbGdjqOjQG_howQ",
+  authDomain: "todo-89f50.firebaseapp.com",
+  projectId: "todo-89f50",
+  storageBucket: "todo-89f50.firebasestorage.app",
+  messagingSenderId: "955544085650",
+  appId: "1:955544085650:web:f172efa4f57af69956f24b",
+  measurementId: "G-EGGLS2CV1E"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default db;
